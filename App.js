f76d00336d75f0ba610fd40a65a6ba7661cd9123
Ashish <ashish@gmail.com>
2025-03-13 23:20:17 +0530

    Episode-2 all about the parcel bundler, node module

--- App.js
+++ App.js
@@ -1,10 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+
 const parent = React.createElement(
     "div",
     {id:"parent"},[
     React.createElement("div",
         {id:"child"},
-        [React.createElement("h1",{},"I am in the h1 tag!"),
-         React.createElement("h2",{},"I am in h2 tag!")
+        [React.createElement("h1",{},"This is the namaste react!"),
+         React.createElement("h2",{},"I am Ashish Shinde!")
         ]),
    React.createElement("div",
     {id:"child2"},
--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 #Namaste React 
 
+# Parcel 
+- Dev build 
+- local server 
+- HMR (Hot Mudule Replacement)
+- File watching algorithm
+- Caching - Faster Builds 
+- Image optmiztion
+- Minification 
+- Bunding 
+- Compress
+- Consistant Hashing 
+- Code Spitting 
+- Differential Bundling (it supports older browser)
+- Dianostic
+- Error handling
+
